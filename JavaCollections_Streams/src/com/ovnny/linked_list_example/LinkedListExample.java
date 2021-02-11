0d4870f85e5cf0513deee45dd8041ed7097d1db4
ovnny <viniciusricardo.ferrera@gmail.com>
2021-02-11 15:44:14 -0300

    Estudando as Queues e seus métodos de funcionamento - poll(), peek(), add(), element(), iterator() and next().

--- JavaCollections_Streams/src/com/ovnny/linked_list_example/LinkedListExample.java
+++ JavaCollections_Streams/src/com/ovnny/linked_list_example/LinkedListExample.java
@@ -1,5 +1,6 @@
 package com.ovnny.linked_list_example;
 
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
@@ -13,6 +14,44 @@ public class LinkedListExample {
         filaBanco.add("Pamela");
         filaBanco.add("Anderson");
 
-        System.out.println(filaBanco);
+        System.out.println(filaBanco); // [Patrícia, Roberto, Flávio, Pamela, Anderson]
+
+        String primeiroDaFila = filaBanco.poll();
+        System.out.println(primeiroDaFila); // Patrícia
+
+        System.out.println(filaBanco); // [Roberto, Flávio, Pamela, Anderson]
+        String primeiroCliente = filaBanco.peek();
+
+        System.out.println(primeiroCliente); // Roberto
+        System.out.println(filaBanco); // [Roberto, Flávio, Pamela, Anderson]
+
+        // filaBanco.clear(); // induzindo o .element() ao erro
+
+        /*
+          Exception in thread "main" java.util.NoSuchElementException
+         */
+
+        String primeiroCLienteOuErro = filaBanco.element();
+        System.out.println(primeiroCLienteOuErro); // Roberto
+        System.out.println(filaBanco); // [Roberto, Flávio, Pamela, Anderson]
+
+        for (String client: filaBanco) {
+            System.out.println(client);
+        } /*
+            Roberto
+            Flávio
+            Pamela
+            Anderson
+        */
+
+        Iterator<String> iteratorFilaBanco = filaBanco.iterator();
+        while (iteratorFilaBanco.hasNext()) {
+            System.out.println("---> " + iteratorFilaBanco.next());
+        }
+
+        System.out.println((filaBanco.size()));
+        System.out.println(filaBanco.isEmpty());
     }
 }
+
+// /home/ovnny/Desktop/github/InterBootcamp-DIO/JavaCollections_Streams/src/com/ovnny
