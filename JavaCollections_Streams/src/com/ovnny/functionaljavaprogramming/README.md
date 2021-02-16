936c8cea5d59c1a45a8511e3607409bdc4b088ca
ovnny <viniciusricardo.ferrera@gmail.com>
2021-02-16 15:33:41 -0300

    Iniciando os estudos em programação funcional do Java, imutabilidade e a API de Threads

---
+++ JavaCollections_Streams/src/com/ovnny/functionaljavaprogramming/README.md
@@ -0,0 +1,28 @@
+## Paradigma Funcional no Java
+
+#### Imutabilidade
+#### Lambda
+#### Recursividade
+
+___
+### Interfaces Funcionais
+
+#### High order functions
+#### Parâmetros de função
+#### Iterações entre funções
+
+___
+### Processamento assíncrono e Paralelo
+
+#### Síncrono e Assíncrono
+
+#### O que é uma thread?
+
+--> subsistema. 
+Forma de um processo se dividir em duas ou mais tarefas que podem ser executadas simultaneamente.
+
+
+#### API Java 8
+#### Parallel Streams
+
+___
---
+++ JavaCollections_Streams/src/com/ovnny/functionaljavaprogramming/threads/ThreadExemplo.java
@@ -0,0 +1,31 @@
+package com.ovnny.functionaljavaprogramming.threads;
+
+public class ThreadExemplo {
+
+    public static void main(String[] args) {
+        BarraDeCarregamento2 barraDeCarregamento2 = new BarraDeCarregamento2();
+        barraDeCarregamento2.start();
+    }
+
+}
+
+class GerarPDF implements  Runnable {
+    @Override
+    public void run() {
+        System.out.println("Gerar PDF");
+    }
+}
+
+class BarraDeCarregamento implements Runnable {
+    @Override
+    public void run() { System.out.println("Loading ..."); }
+}
+
+class BarraDeCarregamento2 extends  Thread {
+    @Override
+     public void run() {
+        super.run();
+        System.out.println("rodei" + this.getName());
+    }
+
+}
