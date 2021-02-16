bf2784b127e9d9ecb614f7c44442884323c3ec91
ovnny <viniciusricardo.ferrera@gmail.com>
2021-02-16 17:35:09 -0300

    Diferenças entre programação imperativa Vs. Declarativa (java Streams e outras coisas

---
+++ javafunctional/src/main/java/imperative/Main.java
@@ -0,0 +1,64 @@
+package imperative;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static imperative.Main.Gender.FEMALE;
+import static imperative.Main.Gender.MALE;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Person> people = List.of(
+            new Person("John", MALE),
+            new Person("Maria", FEMALE),
+            new Person("Aisha", FEMALE),
+            new Person("Alex", MALE),
+            new Person("Alice", FEMALE)
+        );
+
+        System.out.println("Imperative Approach");
+        // Imperative approach
+        List<Person> females = new ArrayList<>();
+
+        for (Person person : people) {
+            if (FEMALE.equals(person.gender)) {
+                females.add(person);
+            }
+        }
+
+        for (Person female : females) {
+            System.out.println(female);
+        }
+// ________________________________________________________________
+        // Declarative approach
+        System.out.println("Declarative approach");
+        List<Person> females2 = people.stream()
+                .filter(person -> FEMALE.equals(person.gender))
+                .collect(Collectors.toList());
+        females2.forEach(System.out::println);
+    }
+
+    static class Person {
+        private final String name;
+        private final Gender gender;
+
+        Person(String name, Gender gender) {
+            this.name = name;
+            this.gender = gender;
+        }
+
+        @Override
+        public String toString() {
+            return "Person{" +
+                    "name='" + name + '\'' +
+                    ", gender=" + gender +
+                    '}';
+        }
+    }
+
+    enum Gender {
+        MALE,
+        FEMALE
+    }
+}
