f639d7133808c107f2317dbd28f77acc37712349
ovnny <viniciusricardo.ferrera@gmail.com>
2021-03-06 12:27:19 -0300

    Bateu na trave, mas não foi dessa vez. Dei uma lida em uns artigos sobre strings e irei tentar por um caminho diferente

--- desafioJavaAvancado/src/com/ovnny/desafiojavaavancado/desafio1/stringchallenge/StringChallenge.java
+++ desafioJavaAvancado/src/com/ovnny/desafiojavaavancado/desafio1/stringchallenge/StringChallenge.java
@@ -16,7 +16,7 @@ enum TextMetadata {
 public class StringChallenge {
     public static void main(String[] args) {
 
-        String previousTokens = "abcdef abc abc abc beef beeef beeeeef beef beef beef cd cd cd cdcd dado dado dd dd    dddob  jklmnopq   klind  quioiuer quioiuer quioiuer zzzzzzzzzzzzzz. jkkkkkkpp juninho    juninho     nhinhoju.";
+        String previousTokens = "banana banana abacate laranja uva uva uva uva ibirut jabuticabaabababa jabuti jabuti abracadabra";
         String[] listTokens = previousTokens.split(" ");
 
         // Push tokens to StringBuilder then sanitize it and collect it to List<String>
@@ -24,7 +24,7 @@ public class StringChallenge {
                 .map(t -> new StringBuilder().append(t).toString())
                 .map(t -> t.replaceAll("\\W", ""))
                 .map(t -> t.replaceAll("[.]", ""))
-                .filter(t -> t.length() > 1)
+                .filter(t -> t.length() >= 1)
                 .collect(Collectors.toList());
 
         Map<String, Integer> wordCollisions = text.stream()
@@ -40,33 +40,31 @@ public class StringChallenge {
         Comparator<Word> byPotentialCompression = new CompareByKeyAndValue();
         wordsList.sort(byPotentialCompression);
 
-        List<String> filteredWords = wordsList.stream()
-                .map(f -> f.name).collect(Collectors.toList());
-
         // Building the hashtable's words
-        Set<String> wordIndex = new HashSet<>();
+        Set<String> chosedWords = new HashSet<>();
 
-        /*  TENTANDO UMA OUTRA ABORDAGEM:
-            PROJETO: FINDER
-              Classe que une duas estruturas de dados diferentes e um iterador observer
-              que observa o gatilho que será baseado nos valores ASCII do alfabeto "toLower()"
-              r"/[a-z*]" --> engatilhado por um "findfirst()" e um loop que ira iterar de
-              n até n + AlPHABET_LENGHT pra cada procura na lista
-          */
+        ListIterator<Word> it = wordsList.listIterator();
+        while(it.hasNext()) {
+            Word word = it.next();
+            chosedWords.add(word.name);
+            if ( it.next().name.charAt(0) > word.name.charAt(0) |
+                word.name.charAt(0) != it.next().name.charAt(0) ) {
+                chosedWords.add(word.name);
+                chosedWords.add(it.next().name);
+
+            } else { it.previous(); }
+        }
 
 
-
-
-
-        TextCompresser compress = new TextCompresser(text, wordIndex);
+        TextCompresser compress = new TextCompresser(text, chosedWords);
         List<String> compressedText = compress.compressText(text);
 
         for (String w: compressedText) {
             System.out.print(w +" ");
         }
         System.out.println();
-        System.out.println(wordIndex.size());
-        wordIndex.stream()
+        System.out.println(chosedWords.size());
+        chosedWords.stream()
                 .sorted(Comparator.naturalOrder())
                 .forEach(w -> System.out.println(w.charAt(0)+". = " + w));
     }
@@ -101,29 +99,31 @@ class TextCompresser {
 class CompareByKeyAndValue implements Comparator<Word> {
     @Override
     public int compare(Word s1, Word s2) {
-        if(     (int)s1.name.charAt(0) < (int)s2.name.charAt(0) &&
-                s1.savedSpace > s2.savedSpace ||
-                (int)s1.name.charAt(0) == (int)s2.name.charAt(0) &&
-                s1.savedSpace >= s2.savedSpace) {
-            return -1;
-        }
-        else if (   (int)s1.name.charAt(0) > (int) s2.name.charAt(0) &&
-                    s1.savedSpace < s2.savedSpace  ||
-                    (int)s1.name.charAt(0) == (int) s2.name.charAt(0) &&
-                    s2.savedSpace <= s1.savedSpace ){
-            return 1;
+        int w1 = (int)s1.name.charAt(0);
+        int w2 = (int)s2.name.charAt(0);
+        int v1 = s1.savedSpace;
+        int v2 = s2.savedSpace;
 
-        } else { return 0; }
+        if( (w2 > w1 & v1 > v2) | (w1 == w2 && v2 <= v1 )  ) {
+            return -1; }
+
+        else if(v1 > v2 & w2 < w1) {
+            return 1; } else {  return 0 ;}
     }
 }
 
 class Word extends CompareByKeyAndValue{
     String name;
     Integer savedSpace;
+    boolean hasMaxCompression = false;
 
     public Word(String name, Integer savedSpace) {
         this.name = name;
         this.savedSpace = savedSpace;
+    }
+
+    public void setHasMaxCompression(boolean hasMaxCompression) {
+        this.hasMaxCompression = hasMaxCompression;
     }
 
     @Override
