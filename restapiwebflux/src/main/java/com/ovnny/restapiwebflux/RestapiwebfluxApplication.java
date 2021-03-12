29874c22cc5c1f582c87f326e0f3a6fdd27165aa
ovnny <viniciusricardo.ferrera@gmail.com>
2021-03-11 21:49:29 -0300

    iniciando o projeto rest api com spring webflux

---
+++ restapiwebflux/src/main/java/com/ovnny/restapiwebflux/RestapiwebfluxApplication.java
@@ -0,0 +1,13 @@
+package com.ovnny.restapiwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiwebfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiwebfluxApplication.class, args);
+	}
+
+}
---
+++ restapiwebflux/src/test/java/com/ovnny/restapiwebflux/RestapiwebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ovnny.restapiwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiwebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
